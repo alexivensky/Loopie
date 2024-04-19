631a47caff3f74a8fecf546ba239c2c0e46370de
alexivensky <AWI4@pitt.edu>
2024-04-18 23:29:59 -0400

    audio thread now actually multi-threads... and distortion POC works

--- UI/loopie_ui.py
+++ UI/loopie_ui.py
@@ -22,35 +22,36 @@ class LoopieUI(QtWidgets.QMainWindow):
         
         self.inputIndex = 0
         self.outputIndex = 0
+        self.inputChannels = min(self.p.get_device_info_by_index(self.inputIndex)['maxInputChannels'], 2)
+        self.outputChannels = min(self.p.get_device_info_by_index(self.outputIndex)['maxOutputChannels'], 2)
         
-        self.stream_in = self.stream_in = self.p.open(format=pyaudio.paInt16,
-                                     channels=1,
-                                     rate=RATE,
-                                     input=True,
-                                     input_device_index=self.inputIndex,
-                                     frames_per_buffer=CHUNK_SIZE)
-        self.stream_out = self.stream_out = self.p.open(format=pyaudio.paInt16,
-                                      channels=1,
-                                      rate=RATE,
-                                      output=True,
-                                      output_device_index=self.outputIndex,
-                                      frames_per_buffer=CHUNK_SIZE)
-        
-        
+        self.stream_in = None
+        self.stream_out = None
         
         self.populateInputOutput()
+        
+        
+        
+        
+        
+        
+        
         
     def populateInputOutput(self):
         for i in range(self.p.get_device_count()):
             device = self.p.get_device_info_by_index(i)
             if device['maxInputChannels']:
                 self.deviceInputsCB.addItem(f"{device['name']}", i)
+                if i == self.inputIndex:  # If this is the selected input device
+                    self.inputChannels = device['maxInputChannels']
             if device['maxOutputChannels']:
                 self.deviceOutputsCB.addItem(f"{device['name']}", i)
+                if i == self.outputIndex:  # If this is the selected output device
+                    self.outputChannels = device['maxOutputChannels']
                 
     def changeInputDevice(self, index):
         self.inputIndex = self.deviceInputsCB.itemData(index)
-        if self.stream_in:
+        if self.stream_in is not None:
             self.stream_in.stop_stream()
             self.stream_in.close()
         self.stream_in = self.p.open(format=pyaudio.paInt16,
@@ -64,7 +65,7 @@ class LoopieUI(QtWidgets.QMainWindow):
     
     def changeOutputDevice(self, index):
         self.outputIndex = self.deviceOutputsCB.itemData(index)
-        if self.stream_out:
+        if self.stream_out is not None:
             self.stream_out.stop_stream()
             self.stream_out.close()
         self.stream_out = self.p.open(format=pyaudio.paInt16,
@@ -78,7 +79,6 @@ class LoopieUI(QtWidgets.QMainWindow):
             
     def toggleAudio(self, value):
         if (value):
-            # open new thread for real-time audio
             self.audio_thread = AudioThread(self.p, self.inputIndex, self.outputIndex, CHUNK_SIZE, RATE)
             self.audio_thread.start()
         else:
--- loopie_audio.py
+++ loopie_audio.py
@@ -1,15 +1,17 @@
-from PyQt6 import QtCore
+from PyQt6 import QtCore, QtWidgets
 import sys
 import numpy as np
 import pyaudio
+import time
 sys.path.append(".")
 
 class AudioThread(QtCore.QThread):
-    def __init__(self, p, input_device_index, output_device_index, chunk_size, rate):
+    finished = QtCore.pyqtSignal()
+    def __init__(self, p, inputIndex, outputIndex, chunk_size, rate):
         super().__init__()
         self.p = p
-        self.input_device_index = input_device_index
-        self.output_device_index = output_device_index
+        self.inputIndex = inputIndex
+        self.outputIndex = outputIndex
         self.chunk_size = chunk_size
         self.rate = rate
         self.running = False
@@ -17,20 +19,23 @@ class AudioThread(QtCore.QThread):
     def run(self):
         self.running = True
         self.stream_in = self.p.open(format=pyaudio.paInt16,
-                                     channels=1,
-                                     rate=self.rate,
-                                     input=True,
-                                     input_device_index=self.input_device_index,
-                                     frames_per_buffer=self.chunk_size)
+                                    channels=1,
+                                    rate=self.rate,
+                                    input=True,
+                                    input_device_index=self.inputIndex,
+                                    frames_per_buffer=self.chunk_size)
         self.stream_out = self.p.open(format=pyaudio.paInt16,
-                                      channels=1,
-                                      rate=self.rate,
-                                      output=True,
-                                      output_device_index=self.output_device_index,
-                                      frames_per_buffer=self.chunk_size)
+                                    channels=1,
+                                    rate=self.rate,
+                                    output=True,
+                                    output_device_index=self.outputIndex,
+                                    frames_per_buffer=self.chunk_size)
         while self.running:
             data = self.stream_in.read(self.chunk_size, exception_on_overflow=False)
+            data = self.apply_distortion(data)
             self.stream_out.write(data)
+        self.finished.emit()
+
 
     def stop(self):
         self.running = False
@@ -38,4 +43,26 @@ class AudioThread(QtCore.QThread):
         self.stream_in.close()
         self.stream_out.stop_stream()
         self.stream_out.close()
+        
+    ### EFFECTS
     
+    def apply_distortion(self, input_data):
+        input_array = np.frombuffer(input_data, dtype=np.int16)
+        distorted_array = np.clip(input_array * 4, -2000.0, 2000.0)
+        return distorted_array.astype(np.int16).tobytes()
+        
+    
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    p = pyaudio.PyAudio()
+    a = AudioThread(p, 1, 3, 16, 44100)
+    for i in range(p.get_device_count()):
+            device = p.get_device_info_by_index(i)
+            print(i, device['name'])
+    a.start()
+    print("hello!")
+    time.sleep(5)
+    print("it's been 5 seconds")
+    time.sleep(5)
+    a.stop()
+    sys.exit(app.exec())
